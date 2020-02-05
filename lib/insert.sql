f3db6ad428807eea10f06d9fcf4c0122e466c985
quadroturkey <reesemb1@live.com>
2020-02-05 15:38:32 -0600

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 8, "male", "brown", "sweet", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 3, "female", "red", "playfull", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 10, "female", "teal", "book-smart", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 57, "male", "black", "tactical", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 26, "female", "white", "bossy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 40, "male", "green", "evil", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 12, "female", "blue", "british", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 100, "male", "brown", "aggressive", 1);
